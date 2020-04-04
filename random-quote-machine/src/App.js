4e1f4e1de3ce20bd6e031caf3320c3106808edc1
arpita tiwari <arpitatiwari309@gmail.com>
2020-04-04 13:39:11 +0530

    random-quote-machine

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -1,63 +1,63 @@
-import React,{Component} from 'react';
-import {random} from 'lodash';
+import React, { useEffect, useState } from 'react';
+import { random } from 'lodash';
 import 'typeface-roboto';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import QuoteMachine from './Components/quoteMachine';
+
 const styles = {
   container: {
     alignItems: 'center',
     display: 'flex',
     height: '100vh',
+    backgroundColor:'pink'
   }
 };
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      quotes:[],
-      selectedQuoteIndex:''
-    }
-    this.assignNewQuoteIndex=this.assignNewQuoteIndex.bind(this);
-    this.selectQuoteIndex=this.generateNewQuoteIndex.bind(this);
-  }
-  componentDidMount(){
-    fetch('https://type.fit/api/quotes')
-    .then(data=>data.json())
-    .then(quotes=>this.setState({quotes},this.assignNewQuoteIndex))
-  }
-  get selectedQuote(){
-    if(!this.state.quotes.length||!Number.isInteger(this.state.selectQuoteIndex))
-    {
+
+function App({ classes }) {
+  const [quotes, setQuotes] = useState([]);
+  const [selectedQuoteIndex, setSelectedQuoteIndex] = useState(null);
+
+  useEffect(async () => {
+    const data = await fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json');
+    const quotes = await data.json();
+    setQuotes(quotes);
+    setSelectedQuoteIndex(random(0, quotes.length - 1));
+  }, []);
+
+  function getSelectedQuote() {
+    if (!quotes.length || !Number.isInteger(selectedQuoteIndex)) {
       return undefined;
     }
-    return this.state.quotes(this.state.selectedQuoteIndex);
+    return quotes[selectedQuoteIndex];
   }
-  assignNewQuoteIndex(){
-    this.setState({selectedQuoteIndex:this.generateNewQuoteIndex()})
-  }
-  generateNewQuoteIndex(){
-    if(!this.state.quotes.length){
-      return;
+
+  /**
+   * Returns an integer representing an index in state.quotes
+   * If state.quotes is empty, returns undefined
+   */
+  function generateNewQuoteIndex() {
+    if (!quotes.length) {
+      return undefined;
     }
-    return random(0,this.state.quotes.length-1)
+    return random(0, quotes.length - 1);
   }
-  render(){
-    
-    return (
-      <Grid className={this.props.classes.container} id="quote-box" justify="center" container>
+
+  function assignNewQuoteIndex() {
+    setSelectedQuoteIndex(generateNewQuoteIndex());
+  }
+
+  return (
+    <Grid className={classes.container} id="quote-box" justify="center" container>
       <Grid xs={11} lg={8} item>
         {
-          this.selectedQuote?
-          <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} /> 
-          :null
-          
+          getSelectedQuote() ?
+          <QuoteMachine selectedQuote={getSelectedQuote()} assignNewQuoteIndex={assignNewQuoteIndex} /> :
+          null
         }
       </Grid>
     </Grid>
-    );
-  }
-  
+  );
 }
 
 export default withStyles(styles)(App);
--- random-quote-machine/src/Components/quoteMachine.js
+++ random-quote-machine/src/Components/quoteMachine.js
@@ -6,25 +6,40 @@ import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faTumblr } from '@fortawesome/free-brands-svg-icons';
+import '../App.css'
+
 
 const QuoteMachine = ({ assignNewQuoteIndex, selectedQuote }) => (
-  <Card>
+  <Card id='card'>
     <CardContent>
       <Typography id="text">
-        {selectedQuote.text} - <span id="author">{selectedQuote.author}</span>
+        {selectedQuote.quote}
+        <div>
+         <span id="author"> - {selectedQuote.author}</span>
+        </div>
+        
       </Typography>
     </CardContent>
     <CardActions>
-      <Button id="new-quote" size="small" onClick={assignNewQuoteIndex}>Next Quote</Button>
       <IconButton
         id="tweet-quote"
         target="_blank"
-        href={encodeURI(`https://twitter.com/intent/tweet?text=${selectedQuote.text}&hashtags=thewebdevcoach`)}
+        href={encodeURI(`https://twitter.com/intent/tweet?text=${selectedQuote.quote}&hashtags=thewebdevcoach`)}
       >
         <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
       </IconButton>
+      <IconButton
+        id="tumblr"
+        target="_blank"
+        href={encodeURI(`https://tumblr.com/intent/tweet?text=${selectedQuote.quote}&hashtags=thewebdevcoach`)}
+      >
+        <FontAwesomeIcon icon={faTumblr}></FontAwesomeIcon>
+      </IconButton>
+      
+      
     </CardActions>
+    <Button id="new-quote" size="small" onClick={assignNewQuoteIndex} >Next Quote</Button>
   </Card>
 );
 
